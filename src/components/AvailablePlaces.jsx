21adb07f0dabaa18d6ae919cff3089c775d5b861
bubsworth <bubbard96@hotmail.com>
2024-01-29 16:52:08 +0000

    How NOT To Send HTTP Requests (And Why It's Wrong)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,6 +6,16 @@ const places = localStorage.getItem("places");
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  fetch("http://localhost:3000/places")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      {
+        setAvailablePlaces(resData.places);
+      }
+    });
+
   return (
     <Places
       title="Available Places"
